ac424b62e57651e3048c9823312fc91b102060e6
Richard <richardbun10@gmail.com>
2021-04-11 00:28:08 -0400

    create modules and optimize structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+
 import { AuthService } from './auth/auth.service';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  constructor(private _authService: AuthService) {}
+export class AppComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private loggingService: LoggingService
+  ) {}
 
-  ngOnInit(): void {
-    this._authService.autoLogin();
+  ngOnInit() {
+    this.authService.autoLogin();
+    this.loggingService.printLog('Hello from AppComponent ngOnInit');
   }
-
- 
-  
 }
